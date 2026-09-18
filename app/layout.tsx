2cc5e34b70a504b78d423d113e9622d68d994a92
HazemAboRia <hazem@users.noreply.github.com>
2026-09-18 19:43:23 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   themeColor: "#6366f1",
 }
 
